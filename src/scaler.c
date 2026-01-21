78e23a29980ec67517cb2442a86aff9b8d113109
Spixa <spixa@github.com>
2026-01-21 17:02:37 +0330

    Remove min-max scaling

--- src/scaler.c
+++ src/scaler.c
@@ -1,27 +1,6 @@
 #include <stdlib.h>
 #include "scaler.h"
 
-// void scale_matrix(Matrix *x, MatScaler *scaler) {
-//     scaler->size = x->cols;
-//     scaler->min = malloc(x->cols * sizeof(double));
-//     scaler->max = malloc(x->cols * sizeof(double));
-
-//     for (int col = 0; col < x->cols; col++) {
-//         scaler->min[col] = INFINITY;
-//         scaler->max[col] = -INFINITY;
-
-//         for (int row = 0; row < x->rows; row++) {
-//             double value = x->data[row][col];
-//             if (value < scaler->min[col]) scaler->min[col] = value;
-//             if (value > scaler->max[col]) scaler->max[col] = value;
-//         }
-
-//         for (int row = 0; row < x->rows; row++) {
-//             x->data[row][col] = scale_value(x->data[row][col], scaler->min[col], scaler->max[col]);
-//         }
-//     }
-// }
-
 void scale_matrix(Matrix *x, MatScaler *scaler) {
     scaler->size = x->cols;
     scaler->median = malloc(x->cols * sizeof(double));
