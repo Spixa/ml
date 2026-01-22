08423aade93bb7bd25d19bf9ebe6eed9a402f013
Spixa <spixa@github.com>
2026-01-22 16:37:58 +0330

    Fix minor spelling mistake

--- demo.sh
+++ demo.sh
@@ -4,4 +4,4 @@ rm -f latest.txt
 SECONDS=0
 ./build/ml data/housing.csv 0.01 10000 0.8 > latest.txt
 
-echo "Results saved in latest.txt! (Took $SECONDS second(s)"
+echo "Results saved in latest.txt! (Took $SECONDS second(s))"
